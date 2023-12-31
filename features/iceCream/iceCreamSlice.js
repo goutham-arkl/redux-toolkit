0673738b65926e416388f430f0b015050771a50d
Goutham A <goutham.arkl@woxro.in>
2023-12-31 09:34:50 +0530

    first commit

---
+++ features/iceCream/iceCreamSlice.js
@@ -0,0 +1,27 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+  numOfIceCream: 20
+};
+
+const iceCreamSlice = createSlice({
+  name: "iceCream",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIceCream--;
+    },
+    restocked: (state, action) => {
+      state.numOfIceCream += action.payload;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase('cake/ordered', (state) => {
+        state.numOfIceCream--;
+      });
+  }
+});
+
+module.exports = iceCreamSlice.reducer;
+module.exports.iceCreamActions = iceCreamSlice.actions;
---
+++ index.js
@@ -0,0 +1,20 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const iceCreamActions = require('./features/iceCream/iceCreamSlice').iceCreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
+console.log('Initial state', store.getState())
+const unSubscribe=store.subscribe(()=>{
+    console.log('Updated state', store.getState())
+})
+
+store.dispatch(fetchUsers())
+
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restoked(3))
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.restocked(3))
+// unSubscribe()
